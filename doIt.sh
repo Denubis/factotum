8a5b80adba3e743c4084406b808e1159c145e27b
Brian Ballsun-Stanton <brian@fedarch.org>
2016-09-18 18:21:48 +1000

    doeeet

--- doIt.sh
+++ doIt.sh
@@ -4,5 +4,15 @@ set -euo pipefail
 ./headlessFactorio/updateFactorio.sh
 ./headlessFactorio/newMap.sh
 ./headlessFactorio/newFactorioPassword.sh
-read -rsp $'Press any key to continue...\n' -n1 key
+
+hold=' '
+printf "Press 'SPACE' to continue or 'CTRL+C' to exit : "
+tty_state=$(stty -g)
+stty -icanon
+until [ -z "${hold#$in}" ] ; do
+    in=$(dd bs=1 count=1 </dev/tty 2>/dev/null)
+done
+stty "$tty_state"
+
+
 ./headlessFactorio/runFactorio.sh
