80e30d56e9c3b38b7b503f4e8ee3912200e3d5fa
Brian Ballsun-Stanton <brian@fedarch.org>
2016-09-21 09:39:43 +1000

    maps

--- newMap.sh
+++ newMap.sh
@@ -11,16 +11,16 @@ cat > mapsettings.json <<- EOM
   "water": "normal",
   "width": 0,
   "height": 0,
-  "starting_area": "very-high",
+  "starting_area": "high",
   "peaceful_mode": false,
   "autoplace_controls":
   {
-    "coal": {"frequency": "very-low", "size": "very-high", "richness": "very-high"},
-    "copper-ore": {"frequency": "very-low", "size": "very-high", "richness": "very-high"},
-    "crude-oil": {"frequency": "very-low", "size": "very-high", "richness": "very-high"},
-    "enemy-base": {"frequency": "very-low", "size": "high", "richness": "very-high"},
-    "iron-ore": {"frequency": "very-low", "size": "very-high", "richness": "very-high"},
-    "stone": {"frequency": "very-low", "size": "very-high", "richness": "very-high"}
+    "coal": {"frequency": "very-low", "size": "high", "richness": "very-high"},
+    "copper-ore": {"frequency": "very-low", "size": "high", "richness": "very-high"},
+    "crude-oil": {"frequency": "very-low", "size": "high", "richness": "very-high"},
+    "enemy-base": {"frequency": "low", "size": "very-high", "richness": "very-high"},
+    "iron-ore": {"frequency": "very-low", "size": "high", "richness": "very-high"},
+    "stone": {"frequency": "very-low", "size": "high", "richness": "very-high"}
   }
 }
 EOM
