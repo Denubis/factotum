497077f59f50ac701e413569be3f1cfe2badc488
kiska <qingbest@fastmail.fm>
2016-09-25 22:13:50 +1000

    added some error checking

--- downloadModpack.sh
+++ downloadModpack.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
 set -euo pipefail
 
+if [ "$#" -ne 2 ]; then
+	if [ -z "$#" ]; then
+	echo -e "command is empty. please enter arguments"
+        exit 1
+	fi
+fi
 rm "$2"
 
 rm -r /opt/factorio/mods
