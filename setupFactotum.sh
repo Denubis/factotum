3ece8b0f648974c40f7da7b092b08e2c7125e8f5
Brian Ballsun-Stanton <brian@fedarch.org>
2016-10-01 13:36:27 +1000

    more fixes

--- setupFactotum.sh
+++ setupFactotum.sh
@@ -3,13 +3,16 @@ set -euo pipefail
 
 sudo apt-get update && sudo apt-get install -y libzmq-dev libevent-dev python-dev python-pip python-dev build-essential
 
-
 pip install --user click ptyprocess clint circus daemonocle requests
- 
-./headlessFactorio/FactoryFactotum install
-./headlessFactorio/FactoryFactotum authenticate
-./headlessFactorio/FactoryFactotum setup
-./headlessFactorio/FactoryFactotum newmap
+
+mkdir -p $HOME/bin
+ln -s $(git rev-parse --show-toplevel)/FactoryFactotum.sh $HOME/bin/FactoryFactotum
+
+
+FactoryFactotum install
+FactoryFactotum authenticate
+FactoryFactotum setup
+FactoryFactotum newmap
 
 
 
