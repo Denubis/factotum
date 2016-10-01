5a1655a5bc5c419e3481da53ad5396592c822888
Brian Ballsun-Stanton <brian@fedarch.org>
2016-10-01 11:50:40 +1000

    prereq

--- setupFactotum.sh
+++ setupFactotum.sh
@@ -3,7 +3,7 @@
 sudo apt-get update && sudo apt-get install -y libzmq-dev libevent-dev python-dev python-pip python-dev build-essential
 
 
-pip install --user click ptyprocess clint circus daemonocle
+pip install --user click ptyprocess clint circus daemonocle requests
  
 ./headlessFactorio/FactoryFactotum install
 ./headlessFactorio/FactoryFactotum authenticate
