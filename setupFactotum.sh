04fab78d5a9fd6fe56218d9eb95011522acf5c79
Brian Ballsun-Stanton <brian@fedarch.org>
2016-10-01 13:44:47 +1000

    more fixes

--- setupFactotum.sh
+++ setupFactotum.sh
@@ -6,9 +6,15 @@ sudo apt-get update && sudo apt-get install -y libzmq-dev libevent-dev python-de
 pip install --user click ptyprocess clint circus daemonocle requests
 
 mkdir -p $HOME/bin
-rm -rf $HOME/bin/FactoryFactotum
 
-ln -s $(git rev-parse --show-toplevel)/FactoryFactotum.py $HOME/bin/FactoryFactotum
+
+ln -sf "$(git rev-parse --show-toplevel)/FactoryFactotum.py" $HOME/bin/FactoryFactotum
+
+if [ -w /opt/factorio ]; then
+	echo "/opt/factorio" > $HOME/.factorioPath
+else
+	echo "$HOME/factorio" > $HOME/.factorioPath
+fi
 
 
 FactoryFactotum install
