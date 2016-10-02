4676e767e7e4adc5b0f7ff3eeaf4ce12713c9ba7
Brian Ballsun-Stanton <brian@fedarch.org>
2016-10-02 23:23:54 +1100

    0.0.15

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ def read(fname):
 
 setup(
     name = "FactoryFactotum",
-    version = "0.0.14",
+    version = "0.0.15",
     author = "Brian Ballsun-Stanton",
     author_email = "factorio@drbbs.org",
     description = ("A tool to control a headless factorio server."),
