3ee0d2ab95cdb57d0747d9aecabf24454bdedaad
Brian Ballsun-Stanton <brian@fedarch.org>
2016-10-03 13:27:00 +1100

    more

---
+++ factotum/__init__.py
@@ -0,0 +1,14 @@
+
+		
+cli.add_command(password)
+cli.add_command(factorio)
+cli.add_command(rcon)
+cli.add_command(update)
+cli.add_command(newMap)
+cli.add_command(install)
+cli.add_command(setup)
+cli.add_command(authenticate)
+
+
+if __name__ == '__main__':
+	cli()    
