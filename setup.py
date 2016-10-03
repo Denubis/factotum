6af4a2d1c8ffde90933b0520a4cd2afda57f40d2
Brian Ballsun-Stanton <brian@fedarch.org>
2016-10-03 12:43:48 +1100

    more broke

--- setup.py
+++ setup.py
@@ -28,7 +28,7 @@ setup(
     include_package_data=True,
     entry_points={
         'console_scripts': [
-            'FactoryFactotum = FactoryFactotum:cli'
+            'FactoryFactotum=FactoryFactotum:cli'
         ]
     }
 
